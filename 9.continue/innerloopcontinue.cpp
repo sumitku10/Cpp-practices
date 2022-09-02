5bc04ceb7b5e91413589549fbd9dd57008006ec3
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-09-02 16:32:46 +0530

    new

--- 9.continue/innerloopcontinue.cpp
+++ 9.continue/innerloopcontinue.cpp
@@ -6,6 +6,7 @@ int main()
    for(int i=1;i<=3;i++){
        for(int j=1;j<=3;j++){
            if(i==2&j==2){
+             // where  2 2 is not print
                continue;
            }
            cout<<i<<" "<<j<<endl;
