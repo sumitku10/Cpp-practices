cef0481cfe42357a2af85ef88500652232e1b20c
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-09-02 16:31:09 +0530

    new

--- 9.continue/continue.cpp
+++ 9.continue/continue.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 #include<string>
-int main()
-{
+int main(){
    for(int i=1;i<=5;i++){
        if(i==3){
+        // where three is not printing
            continue;
        }
        cout<<i<<endl;
