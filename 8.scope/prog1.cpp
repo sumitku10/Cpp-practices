4fc2aca236ad126ad2974d65ec118c0f950a3cdf
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-09-02 16:29:36 +0530

    new

--- 8.scope/prog1.cpp
+++ 8.scope/prog1.cpp
@@ -3,10 +3,8 @@ using namespace std;
 #include <string>
 int main()
 {
-    int i, x = 10;
-    for (i = 0; i < 5; i++)
-    {
-
+    int i , x = 10;
+    for (i = 1; i < 5; i++){
         int x = i;
         cout << x << endl;
     }
