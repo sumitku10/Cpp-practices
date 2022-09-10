1c9f5d83fd9a33d6379be6b842006d9bb0ea6391
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-09-10 18:55:14 +0530

    new

---
+++ 19.polymorphism/function_overloading/add.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void funa(int x){
+    cout<<x;
+
+};
+int main(){
+    int a=10,b=20;
+    double a=30;
+    int sum=a+b;
+    funa(sum);
+
+
+ return(0);
+}
