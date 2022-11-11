13a5aa65a00d2971c675065961a1fd14c3b843c2
Sumit Kumar <108218318+sumitku10@users.noreply.github.com>
2022-11-12 00:47:54 +0530

    new

--- 19.polymorphism/operater_overloading/binary+operator.cpp
+++ 19.polymorphism/operater_overloading/binary+operator.cpp
@@ -7,25 +7,23 @@ private:
     float real, imag;
 
 public:
-    complex()
-    {
+    complex(){
         real = 0.0;
         imag = 0.0;
     }
-    void input()
-    {
+    void input(){
+        cout<<"Enter the real part : ";
         cin >> real;
+        cout<<"Enter the imag part : ";
         cin >> imag;
     }
-    complex operator+(const complex &obj)
-    {
+    complex operator+(const complex &obj){
         complex temp;
         temp.real = real + obj.real;
         temp.imag = imag + obj.imag;
         return temp;
     }
-    void output()
-    {
+    void output(){
         if (imag < 0)
             cout << real << imag << endl;
         else
